122f815f0b1b0131a4f070772a43ebc0a596e88f
Neelansh Jaiswal <119448065+ajneelansh@users.noreply.github.com>
2024-04-21 11:13:38 +0530

    Update Login_page.dart

--- Login_page.dart
+++ Login_page.dart
@@ -1,1 +1,24 @@
-void run => myapp()
+import 'package:animation/ui/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        accentColor: Color.fromRGBO(209, 28, 121, 1.0),
+        primaryColor: Color.fromRGBO(251, 177, 21, 1.0),
+      ),
+      home: LoginPage(),
+    );
+  }
+}
