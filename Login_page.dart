cd591b5e92ed02cb26c954e61f9cfe96c05ba01d
Neelansh Jaiswal <119448065+ajneelansh@users.noreply.github.com>
2024-05-01 14:54:28 +0530

    Update Login_page.dart

--- Login_page.dart
+++ Login_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 
 void main() {
   SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(MyApp());
 }
 
